f1854fb381ecb821331001f2f663a073e67a971f
Nikolas <ZaL1Vv2003@gmail.com>
2023-04-12 11:44:19 +0200

    connects postgresql to django

--- clothing_store/settings.py
+++ clothing_store/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'clothing_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clothing_store',
+        'USER': 'bob',
+        'PASSWORD': '228',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
