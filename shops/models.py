3c62f80de9795648d16c07eef448427e103b2e8b
Nikolas <ZaL1Vv2003@gmail.com>
2023-04-13 11:55:53 +0200

    updates the shop model

--- shops/models.py
+++ shops/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 class Shop(models.Model):
+    branch = models.CharField(max_length=256, unique=True)
     address = models.ForeignKey('address.Address', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.branch} --- {self.address}'
